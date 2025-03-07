4dd483a7fad3adf4368c70a0c9bcd9d009bd5bd9
HP <HP@LAPTOP-P7UD8B89>
2025-03-07 15:40:59 +0700

    Add project files.

---
+++ lab4/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Task
+    {
+        protected string description;
+
+        public Task() { }
+        {
+        this.setDescription("Default task");
+        }
+        public Task(string description )
+            { this.description = description; }
+
+    public string getDescription()
+        {
+            return description;
+        }
+    public void setDescription (string description)
+        {
+            this.description = description;
+            {
+                Public 
+            }
+
+        }
+    }
+}
---
+++ lab4/ToDoList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class ToDoList
+    {
+        List<Task> tasks = new List<Task>();
+        public void insertTask (Task task)
+        {
+            tasks.Add(task);
+        }
+        public void displayTasks()
+        {
+            int i = 1;
+            foreach (Task task in tasks) {
+                Console.Write(i + ": ");
+                Console.WriteLine(task.getDescription);
+        }
+    }
+}
